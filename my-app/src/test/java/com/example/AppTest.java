de1dfb95063ff7acdbd6a920857c7a935b4600e9
Adarsh <reddyadarsh164@gmail.com>
2025-01-16 18:31:14 -0600

    Added JUNIT and MAven for the project

---
+++ my-app/src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test
+    void testAppGreeting() {
+        Main app = new Main();
+        assertEquals("Hello, World!", app.getGreeting(), "App should return greeting message");
+    }
+}
